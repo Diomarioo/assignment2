c14bb5e28c47265e64303bf9d18737a76efee555
Dias Kuspan <dias200620@gmail.com>
2025-03-05 23:43:03 +0500

    book repository

---
+++ src/main/java/com/example/repository/BookRepository.java
@@ -0,0 +1,21 @@
+package com.example.repository;
+
+import com.example.model.Book;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class BookRepository {
+
+    private final List<Book> books = new ArrayList<>();
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public List<Book> getAllBooks() {
+        return books;
+    }
+}
